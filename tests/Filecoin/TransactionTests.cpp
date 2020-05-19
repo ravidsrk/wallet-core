d0d1abd80ba6e50db59fcf665b8cb9bbb0819bb7
Catenocrypt <catenocrypt@users.noreply.github.com>
2020-05-20 00:37:51 +0200

    Test for Filecoin fix.

--- tests/Filecoin/TransactionTests.cpp
+++ tests/Filecoin/TransactionTests.cpp
@@ -33,4 +33,24 @@ TEST(FilecoinTransaction, Serialize) {
               "0171a0e4022020421f7d6207c9575803d5d96387c399acbf4bdac3026196cb4d423bc8966bb1");
 }
 
+TEST(FilecoinTransaction, SerializeWith0) {
+    const PrivateKey privateKey(
+        parse_hex("2f0f1d2c8de955c7c3fb4d9cae02539fadcb13fa998ccd9a1e871bed95f1941e"));
+    const auto publicKey = privateKey.getPublicKey(TWPublicKeyTypeSECP256k1Extended);
+    const Address fromAddress(publicKey);
+    const Address toAddress("f1hvadvq4rd2pyayrigjx2nbqz2nvemqouslw4wxi");
+
+    Transaction tx(toAddress, fromAddress,
+                   /*nonce*/ 0x1234567890,
+                   /*value*/ 0,
+                   /*gasPrice*/ 1,
+                   /*gasLimit*/ 50);
+
+    ASSERT_EQ(hex(tx.message().encoded()),
+              "8855013d403ac3911e9f806228326fa68619d36a4641d455013d413d4c3fe3d89f99495a48c6046224a7"
+              "1f0cd71b0000001234567890404200014200320040");
+    ASSERT_EQ(hex(tx.cid()),
+              "0171a0e40220772adc479b71ce1ac9478c546fc8800f180801d717e6b942c5f6082c93fea54b");
+}
+
 } // namespace TW::Filecoin
